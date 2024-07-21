75cdef8c8e8327c090a8c8bd0e73b19acce18409
RODHOLFO_EBAC <ro.garo87@gmail.com>
2024-07-21 11:33:40 -0300

    Entrega do Exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,33 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar dois comentários", () => {
+    const { debug } = render(<PostComment />);
+
+    // adiciona o primeiro comentário
+    fireEvent.change(screen.getByTestId("texto-comentario"), {
+      target: {
+        value: "Meu Primeiro Comentario",
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-comentario"));
+
+    // adiciona o segundo comentário
+    fireEvent.change(screen.getByTestId("texto-comentario"), {
+      target: {
+        value: "Meu Segundo Comentario",
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-comentario"));
+
+    debug();
+    expect(screen.getAllByTestId("total-comentario")).toHaveLength(2);
+  });
 });
